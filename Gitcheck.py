b41e914cf3217f5fcce69d8c08e8c3fbf1cbd750
doop <75194878+u6m@users.noreply.github.com>
2021-04-24 15:26:15 +0100

    Source

---
+++ Gitcheck.py
@@ -0,0 +1,44 @@
+import requests, re, json, time, os
+from colorama import init, Fore
+init(autoreset=True)
+
+os.system('cls')
+
+def check(username):
+    s = requests.Session()
+
+    r = s.get("https://github.com/" + username)
+    if (r.status_code == 404):
+        return ("hit")
+    elif (r.status_code == 200):
+        return ("taken")
+    elif (r.status_code == 429):
+        return ("timeout")
+    else:
+        return ("Unknown Error")
+
+
+if __name__ == "__main__":
+    with open('words.txt') as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    for word in content:
+        file = open('hits.txt', 'a')
+        resp = check(word)
+        if resp == "hit":
+            print(Fore.GREEN + 'HIT: ' + Fore.RESET + Fore.GREEN + word)
+            file.write(word + "\n")
+            file.close()
+        elif resp == "taken":
+            print(Fore.RED + 'BAD: ' + Fore.RESET + Fore.RED + word)
+        elif resp == "timeout":
+            print(Fore.WHITE + "Timeout")
+            time.sleep(1000)
+        else:
+            print(Fore.YELLOW + "UNKNOWN ERROR - CODE PROBLEM?")
+            time.sleep(1000)
+print(f"{Fore.RESET}[{Fore.YELLOW}CHECKER FINISHED - HITS SAVED TO FILE{Fore.RESET}]")
+print(" ")
+print(f"{Fore.RESET}[{Fore.GREEN}Created by github.com/u6m ~ doop{Fore.RESET}]")
+file.close()
+
